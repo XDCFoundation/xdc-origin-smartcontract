7447883567c971a97f53ac9460d7f870d327e7f3
appsstore <revathi@leewayhertz.com>
2021-08-17 18:39:51 +0530

    adding redirection

---
+++ config/auth.js
@@ -0,0 +1,41 @@
+module.exports = {
+
+  'githubAuth': {
+    'clientID': 'c455ac87f1a99c52cde0',
+    'clientSecret': 'dd91931ce6cebad79d6a035ace4a7fe2d595bc85',
+    'callbackURL': '/auth/github/callback'
+  },
+
+  'googleAuth': {
+    'clientID': '698754492059-v4ist2ra5a43klek8patiesthmb20gqe.apps.googleusercontent.com',
+    'clientSecret': 'CNyppdWZIF79bDX6o4W2PixS',
+    'callbackURL': '/auth/google/callback'
+  },
+
+  'facebookAuth' : {
+    'clientID'      : '257057545245950', // your App ID
+    'clientSecret'  : '0134a4bf8842cca0f616bee3b30f8803', // your App Secret
+    'callbackURL'   : '/auth/facebook/callback'
+},
+
+  'nodemailerAuth': {
+    'user': 'contract@mail-b01.cloudmailbox.in',
+    'pass': 'Hak-ess@2018**',
+  },
+
+  'jwtAuthKey': {
+    'secret': "heyitsmeakshayhere",
+    'tokenLife': 60 * 30
+  },
+
+  'binanceKey': {
+    'apiKey': '8DFjjeVS0JhzVVcXuvUenxhDYBoO2OoAIJpCXJpy19rX1aaSfk6SZ9CiGFLiLTZI',
+    'apiSecret': 'Cq0xxr0UPGSZOAKbvB6YbrueutUCSf2QrIyephBWZbOAkcBuOQd0tTwzat8MqsuR'
+  },
+  'paypal': {
+    'mode': 'live',
+    'client_id': 'AcYZI1aF91ZLF_SimfdvKZrgKtgB2dwipISU1mq1tV-Upj3S63B3Z6S1Q1a8SUBhdrxeX1dXxyy3Ytw-',
+    'client_secret': 'EJF-gFuJuzkkQ2a78Y1tFtjbfTGtq9IKc2aZ-qc11scF2MF152lnYlBrn6xOC9eCVVF0xYUGmfiI5Akn'
+  }
+
+};
