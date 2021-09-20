038e0475d8c96fcbb59ecd729c27646902e22e10
shivanib <shivanib@leewayhertz.com>
2021-09-20 19:01:38 +0530

    Github login issue x3 (updating with the local configuration)

--- config/passport.js
+++ config/passport.js
@@ -291,7 +291,7 @@ module.exports = function (passport) {
   passport.use(new GitHubStrategy({
     clientID: configAuth.githubAuth.clientID,
     clientSecret: configAuth.githubAuth.clientSecret,
-    callbackURL: configAuth.githubAuth.callbackURL, //'http://localhost:4000/auth/github/callback' for local
+    callbackURL: 'http://localhost:4000/auth/github/callback', //for live --> configAuth.githubAuth.callbackURL
     scope: 'user:email'
   },
     function (token, refreshToken, profile, done) {
