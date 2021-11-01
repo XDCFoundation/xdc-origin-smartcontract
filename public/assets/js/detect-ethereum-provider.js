333fc21efeaed3be2bc098de9dafe78756129bb1
shivanib <shivanib@leewayhertz.com>
2021-11-01 23:58:15 +0530

    Some bug fixes and attempts to fix the metamask issue

--- public/assets/js/detect-ethereum-provider.js
+++ public/assets/js/detect-ethereum-provider.js
@@ -1,7 +1,18 @@
-async function ethereumProviders() {
-    const detectEthereumProvider = require('@metamask/detect-provider');
-    window.ethereum.enable();
-    return await detectEthereumProvider();
-}
-
-console.log('detectEthereumProvider is starteeeeeeeeed');
+// async function ethereumProviders() {
+//
+// }
+//
+// const detectEthereumProvider = require('@metamask/detect-provider');
+// window.ethereum.enable();
+// const provider = detectEthereumProvider();
+// if (provider) {
+//     // From now on, this should always be true:
+//     // provider === window.ethereum
+//     // startApp(provider); // initialize your app
+//     console.log("provider =========", provider);
+// } else {
+//     console.log('Please install MetaMask!');
+// }
+// // return await detectEthereumProvider();
+//
+// console.log('detectEthereumProvider is starteeeeeeeeed');
